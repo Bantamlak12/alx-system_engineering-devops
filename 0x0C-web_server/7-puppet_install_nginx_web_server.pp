097f6e8c5c537fe57338758872c29302f5319bd0
Bantamlak <neoluka315@gmail.com>
2023-03-30 00:52:04 +0000

    task 5: first commit

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,32 @@
+# Install and configure Nginx web server using puppet
+
+# update apt packages
+exec { 'update packages':
+  command => '/usr/bin/apt-get update',
+}
+
+# Install Nginx package
+package { 'install nginx':
+  ensure => installed,
+}
+
+# Create configuration file
+file { '/var/www/index.html':
+  content => 'Hello World!'
+}
+
+# redirect
+exec {'redirect_me':
+  command  => 'sed -i "24i\	rewrite ^/redirect_me https://www.alxafrica.com/;" /etc/nginx/sites-available/default',
+  provider => 'shell'
+}
+
+# create symbolic link to enable the virtual host
+file { '/etc/nginx/sites-enabled/default':
+  ensure => 'link',
+}
+
+#Restart the Nginx service
+service { 'nginx':
+  ensure  => running
+}
