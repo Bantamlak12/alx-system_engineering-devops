f3b5cfac356e515ddf70540b2adba3e4f08928a0
Bantamlak <bantamlak29@gmail.com>
2023-05-22 19:08:30 +0300

    task 0: gather data from an api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" A Python script that, using this https://jsonplaceholder.typicode.com/
+    REST API for a given employee ID, returns information about his/her
+    TODO listprogress.
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    # Get employee id from command line
+    employee_id = sys.argv[1]
+
+    # Get information about the employee
+    response = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                            .format(employee_id))
+    if response.status_code == 200:
+        employee_data = response.json()
+        employee_name = employee_data['name']
+
+    # Get information about the employee todo list
+    r = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
+                     .format(employee_id))
+    if r.status_code == 200:
+        todo_list = r.json()
+
+    # Get number of total tasks
+    total_tasks = len(todo_list)
+
+    # Get list of completed tasks
+    completed_tasks = []
+    for task in todo_list:
+        if task['completed']:
+            completed_tasks.append(task)
+    # Get number of completed tasks
+    total_completed_tasks = len(completed_tasks)
+
+    # Print out his/her todo list progress
+    print("Employee {} is done with tasks({}/{})"
+          .format(employee_name, total_completed_tasks, total_tasks))
+    for task in completed_tasks:
+        print("\t {}".format(task.get('title')))
