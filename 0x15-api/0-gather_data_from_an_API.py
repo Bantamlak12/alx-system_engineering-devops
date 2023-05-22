df354ab95d966a52a976221dbec22d62ec37b513
Bantamlak <bantamlak29@gmail.com>
2023-05-22 20:46:14 +0300

    task 0: updated lines

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,7 +9,7 @@ import sys
 
 if __name__ == "__main__":
     # Get employee id from command line
-    employee_id = sys.argv[1]
+    employee_id = int(sys.argv[1])
 
     url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
 
@@ -21,19 +21,15 @@ if __name__ == "__main__":
     response = requests.get("{}/todos".format(url))
     todo_list = response.json()
 
-    # Get number of total tasks
-    total_tasks = len(todo_list)
-
     # Get list of completed tasks
     completed_tasks = []
     for task in todo_list:
         if task.get('completed') is True:
             completed_tasks.append(task)
-    # Get number of completed tasks
-    total_completed_tasks = len(completed_tasks)
 
     # Print out his/her todo list progress
     print("Employee {} is done with tasks({}/{}):"
-          .format(employee_name, total_completed_tasks, total_tasks))
+          .format(employee_name, len(completed_tasks), len(todo_list)))
+    
     for task in completed_tasks:
         print("\t {}".format(task.get('title')))
