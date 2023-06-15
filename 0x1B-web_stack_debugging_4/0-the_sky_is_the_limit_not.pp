4d5f751d4f5a6b19ebf5198314b140c5e1ee5271
Bantamlak <bantamlak29@gmail.com>
2023-06-15 10:20:03 +0300

    updated

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,11 @@
 # Setting the limit from 15 to 4096 in the file /etc/default/nginx
 
-exec {'Substitute root path':
+exec {'increase_the_limit':
   command => "sed -i 's/15/4096/' /etc/default/nginx",
   path    => '/bin',
 } ->
 
-exec {'nginx restart':
+exec {'nginx_restart':
   command => 'sudo service nginx restart',
   path    => '/usr/bin:/usr/sbin',
 }
