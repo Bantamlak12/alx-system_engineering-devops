6e36792ff4dca14df70849e417a7292a5ce16b31
Bantamlak <bantamlak29@gmail.com>
2023-06-15 09:56:15 +0300

    set the limit to 4096

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,9 @@
-# Substituting `root /usr/share/nginx/html;` with `root /var/www/html;`
+# Setting the limit from 15 to 4096 in the file /etc/default/nginx
 
 exec {'Substitute root path':
-  command => "sed -i 's#root /usr/share/nginx/html;#root /var/www/html;#' /etc/nginx/sites-available/default" ,
+  command => "sed -i 's/15/4096/' /etc/default/nginx",
   path    => '/bin',
-}
+} ->
 
 exec {'nginx restart':
   command => 'sudo service nginx restart',
