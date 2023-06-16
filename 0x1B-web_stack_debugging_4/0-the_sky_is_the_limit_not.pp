29be211b9deb9e8de8b9a1373b938e61a9794ce4
Bantamlak <bantamlak29@gmail.com>
2023-06-16 09:01:40 +0300

    puppet -lint style

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,10 @@
 # Setting the limit from 15 to 4096 in the file /etc/default/nginx
-
-exec {'increase_the_limit':
+exec { 'increase_the_limit':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin',
+  path    => '/bin',
 } ->
-
 # Restarting nginx
-exec {'nginx_restart':
-  command => 'nginx restart',
-  path    => '/etc/init.d/:/bin',
+exec { 'nginx_restart':
+  command => 'sudo service nginx restart',
+  path    => '/usr/bin',
 }
